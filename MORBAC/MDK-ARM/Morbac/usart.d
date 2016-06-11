f6a3c5075f3c98273db086eef0d3e62c301797df
Hannes Paulsson <hannespa@kth.se>
2016-06-11 14:04:45 +0200

    morc

--- MORBAC/MDK-ARM/Morbac/usart.d
+++ MORBAC/MDK-ARM/Morbac/usart.d
@@ -8,14 +8,14 @@ morbac\usart.o: ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h
 morbac\usart.o: ../Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h
 morbac\usart.o: ../Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h
 morbac\usart.o: ../Drivers/CMSIS/Include/core_cm0plus.h
-morbac\usart.o: C:\Program Files (x86)\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+morbac\usart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 morbac\usart.o: ../Drivers/CMSIS/Include/core_cmInstr.h
 morbac\usart.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 morbac\usart.o: ../Drivers/CMSIS/Include/core_cmFunc.h
 morbac\usart.o: ../Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h
 morbac\usart.o: ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h
 morbac\usart.o: ../Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-morbac\usart.o: C:\Program Files (x86)\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+morbac\usart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 morbac\usart.o: ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h
 morbac\usart.o: ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h
 morbac\usart.o: ../Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h
